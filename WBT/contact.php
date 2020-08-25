029c25ae9b84b31bfa7fb0885f32eda95d024311
codingworki <67830510+codingworki@users.noreply.github.com>
2020-08-25 12:40:19 +0300

    Update contact.php

--- WBT/contact.php
+++ WBT/contact.php
@@ -22,4 +22,10 @@ foreach($arr as $key => $value) {
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}", "r");
 
+if ($sendToTelegram) {
+    echo("All Good");
+} else {
+    echo("Error");
+}
+
 ?>

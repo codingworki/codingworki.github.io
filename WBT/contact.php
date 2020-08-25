4d673f769f3b080c0b5c8ed2a84841eafbc8eac0
codingworki <67830510+codingworki@users.noreply.github.com>
2020-08-25 10:58:55 +0300

    Update contact.php

--- WBT/contact.php
+++ WBT/contact.php
@@ -1,76 +1,13 @@
-<?php
+<?php 
 
-$post = (!empty($_POST)) ? true : false;
+$recepient = "danchik_fedorov@mail.ru";
+$sitename = "WBT";
 
-if($post)
-{
-$email = trim($_POST['user_email']);
-$name = htmlspecialchars($_POST['user_name']);
-$email = htmlspecialchars($_POST['user_email']);
-$message = htmlspecialchars($_POST['message']);
-$tel = htmlspecialchars($_POST["user_phone"]);
-$error = '';
+$name = trim($_POST["user_name"]);
+$phone = trim($_POST["user_phone"]);
+$email = trim($_POST["user_email"]);
+$message = "Имя: $name \n Телефон: $phone \n Почта: $email";
 
-if(!$name)
-{
-$error .= 'Пожалуйста введите ваше имя<br />';
-}
-
-// Проверка телефона
-function ValidateTel($valueTel)
-{
-$regexTel = "/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/";
-if($valueTel == "") {
-return false;
-} else {
-$string = preg_replace($regexTel, "", $valueTel);
-}
-return empty($string) ? true : false;
-}
-if(!$email)
-{
-$error .= "Пожалуйста введите email<br />";
-}
-if($email && !ValidateTel($email))
-{
-$error .= "Введите корректный email<br />";
-}
-if(!$error)
-
-// (length)
-if(!$message || strlen($message) < 1)
-{
-$error .= "Введите ваше сообщение<br />";
-}
-if(!$error)
-{
-
-
-$name_tema = "=?utf-8?b?". base64_encode($name) ."?=";
-
-$subject ="Новая заявка с сайта domain.name";
-$subject1 = "=?utf-8?b?". base64_encode($subject) ."?=";
-/*
-$message ="\n\nСообщение: ".$message."\n\nИмя: " .$name."\n\nТелефон: ".$tel."\n\n";
-*/
-$message1 ="\n\nИмя: ".$name."\n\nТелефон: " .$tel."\n\nE-mail: " .$email."\n\nСообщение: ".$message."\n\n";	
-
-
-$header = "Content-Type: text/plain; charset=utf-8\n";
-
-$header .= "From: Новая заявка <danchik_fedorov@mail.ru>\n\n";	
-$mail = mail("danchik_fedorov@mail.ru", $subject1, iconv ('utf-8', 'windows-1251', $message1), iconv ('utf-8', 'windows-1251', $header));
-
-if($mail)
-{
-echo 'OK';
-}
-
-}
-else
-{
-echo '<div class="notification_error">'.$error.'</div>';
-}
-
-}
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
 ?>

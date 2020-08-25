061ab6fc388330cd4f79bd9ae4343921d2452df7
codingworki <67830510+codingworki@users.noreply.github.com>
2020-08-25 20:07:38 +0300

    Update contactform.js

--- WBT/contactform.js
+++ WBT/contactform.js
@@ -1,16 +1,17 @@
-jQuery(document).ready(function() {
-    jQuery("#form").submit(function() {
-        let form_data = jQuery(this).serialize();
-        jQuery.ajax({
+$(document).ready(function() {
+    $("#form").submit(function() {
+        let form_data = $(this).serialize();
+        let formNm = $('#' + formID);
+        $.ajax({
             type: "POST",
             url: "contact.php",
-            data: form_data,
+            data: form_data.serialize(),
             success: swal({
                 title: "Спасибо за заявку!",
                 type: "success",
                 showConfirmButton: false,
                 timer: 2000
-            })
+            });
         });
         $(this).find('input,textarea').prop('disabled', true);
         event.preventDefault();

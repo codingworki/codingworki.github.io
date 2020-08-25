facc3e8fba2bb9ab13d2bc5e0b40cb72ccd9b82d
codingworki <67830510+codingworki@users.noreply.github.com>
2020-08-25 10:18:46 +0300

    Add files via upload

---
+++ WBT/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.header_burger').click(function(event){
+         $('.header_burger,.header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+
+$(.'carousel').carousel({
+    interval: 3000
+})
+

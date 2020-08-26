f6f0e5b9e34bca28c0bfbc5ac660565f17f2fa2c
codingworki <67830510+codingworki@users.noreply.github.com>
2020-08-26 08:09:18 +0300

    Update contact.php

--- WBT/contact.php
+++ WBT/contact.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-if (!empty($_POST['user_name']) && !empty($_POST['user_mail'])){
+if (!empty($_POST['user_name']) && !empty($_POST['user_phone'])){
   if (isset($_POST['user_name'])) {
     if (!empty($_POST['user_name'])){
   $name = strip_tags($_POST['user_name']);
@@ -8,56 +8,41 @@ if (!empty($_POST['user_name']) && !empty($_POST['user_mail'])){
   }
 }
  
-
-if (isset($_POST['user_mail']) ) {
+if (isset($_POST['user_mail'])) {
   if (!empty($_POST['user_mail'])){
-  $email = strip_tags($_POST['mail']);
-  $emailFieldset = "Email: ";
+  $phone = strip_tags($_POST['user_mail']);
+  $phoneFieldset = "Email: ";
   }
 }
-    
-    if (isset($_POST['user_phone']) ) {
+if (isset($_POST['user_phone'])) {
   if (!empty($_POST['user_phone'])){
-  $phone = strip_tags($_POST['phone']);
-  $phoneFieldset = "Phone: ";
+  $phone = strip_tags($_POST['user_phone']);
+  $phoneFieldset = "Телефон: ";
   }
 }
-
-
-$nameFieldset = $_POST['user_name'];
-$phoneFieldset = $_POST['user_phone'];
-$emailFieldset = $_POST['user_mail'];
-
-$token = "997818438:AAFD7nf4U7Nc3BFkunTAiOtvGnyHSvi5znw";
-$shat_id = "-1001469624549";
-$sitename = "WBT";
-
-$arr = array (
-
-    'Заказ с сайта: ' => $sitename,
-    'Имя: ' => $nameFieldset,
-    'Телефон: ' => $phoneFieldset,
-    'Почта: ' => $emailFieldset
+$token = "1278144247:AAHfIVMDo0kjHECWc56ykcJKYYIOWUOvB3k";
+$chat_id = "-1001155000770";
+$arr = array(
+  $nameFieldset => $name,
+  $emailFieldset => $email,
+  $phoneFieldset => $phone
 );
-
 foreach($arr as $key => $value) {
-    $txt .= "<b>".$key. "</b> ".$value."%0A";
+  $txt .= "<b>".$key."</b> ".$value."%0A";
 };
-
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}", "r");
-
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 if ($sendToTelegram) {
   
-  echo '<p>Спасибо за отправку вашего сообщения!</p>';
+  echo '<p class="success">Спасибо за отправку вашего сообщения!</p>';
     return true;
 } else {
-  echo '<p><b>Ошибка. Сообщение не отправлено!</b></p>';
+  echo '<p class="fail"><b>Ошибка. Сообщение не отправлено!</b></p>';
 }
 } else {
-  echo '<p>Ошибка. Вы заполнили не все обязательные поля!</p>';
+  echo '<p class="fail">Ошибка. Вы заполнили не все обязательные поля!</p>';
 }
 } else {
-header ("Location: ");
+header ("Location: /");
 }
-
+ 
 ?>
